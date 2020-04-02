a9b328e33eb5d620d75ebc993a09a8a39b8c557b
Wonjun Lee <arm04092@gmail.com>
2020-04-02 18:58:52 +0900

    EF 추가, Northwind Entity model 추가
    HomeController에서 Home/index에 테이블 목록, 카운터 추가
    
    MVC 스캐폴드 활용하여 OrdersController, View 추가
    ( Orders 테이블에 대한 CRUD 코드 자동 추가됨)

--- Northwind/Northwind/Controllers/HomeController.cs
+++ Northwind/Northwind/Controllers/HomeController.cs
@@ -8,31 +8,107 @@ using System.Data;
 using System.Xml.Linq;
 using System.Data.SqlClient;
 using System.Configuration;
+using System.Data.Linq;
+
+using System.Data.Entity;
+using Northwind.Models;
 
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
+        private NorthwindEntities1 db = new NorthwindEntities1();
+
         public ActionResult Index()
         {
-            DataSet ds = new DataSet();
-
-            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString))
+            
+            try
             {
-                try
-                {
-                    connection.Open();
-                    
+                List<string> names = new List<string>();
+                
+                names.Add("Employees");
+                names.Add("Categories");
+                names.Add("Customers");
+                names.Add("Shippers");
+                names.Add("Suppliers");
+                names.Add("Orders");
+                names.Add("Products");
+                names.Add("Order Details");
+                names.Add("Region");
+                names.Add("Territories");
 
-                }
-                catch(Exception ex)
-                {
-                    System.Diagnostics.Trace.WriteLine(ex.Message);
-                }
-                finally
-                {
-                    connection.Close();
-                }
+                List<int> rows = new List<int>();
+
+                
+                var CountQuery = (
+                from customer in db.Employees
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+
+                CountQuery = (
+                from customer in db.Categories
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+
+                CountQuery = (
+                from customer in db.Customers
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Shippers
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Suppliers
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Orders
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Products
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Order_Details
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Region
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+                CountQuery = (
+                from customer in db.Territories
+                select customer
+                ).Count();
+
+                rows.Add(CountQuery);
+
+                ViewBag.Names = names.ToArray();
+                ViewBag.Rows = rows.ToArray();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.WriteLine(ex.Message);
             }
 
             return View();
