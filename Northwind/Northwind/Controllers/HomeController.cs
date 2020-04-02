4d047ef390a0452ba00e7a6662d1697c132d0ca2
Wonjun Lee <arm04092@gmail.com>
2020-04-02 10:11:33 +0900

    DB 연결 설정
    Web.config ConnectionStrings 추가
    Index - table 추가

--- Northwind/Northwind/Controllers/HomeController.cs
+++ Northwind/Northwind/Controllers/HomeController.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using System.Data;
+using System.Xml.Linq;
+using System.Data.SqlClient;
+using System.Configuration;
+
 namespace Northwind.Controllers
 {
     public class HomeController : Controller
     {
         public ActionResult Index()
         {
+            DataSet ds = new DataSet();
+
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["DB"].ConnectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    
+
+                }
+                catch(Exception ex)
+                {
+                    System.Diagnostics.Trace.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
             return View();
         }
     }
