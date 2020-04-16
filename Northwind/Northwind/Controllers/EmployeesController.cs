a42bde8c9e8df6f727ee65d6c2bf257a856e273c
Wonjun Lee <arm04092@gmail.com>
2020-04-16 18:23:48 +0900

    Employees Details, Create 기능 추가
    - Employees Details 컨트롤, 뷰 작업
    - blob 를 byte array에 저장하여 image file로 write하여 return하는 getImg 메소드 작성
    - Employees Create 컨트롤, 뷰 작업
     => 사진 blob 데이터 업로드 추가 해야됨

--- Northwind/Northwind/Controllers/EmployeesController.cs
+++ Northwind/Northwind/Controllers/EmployeesController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Net;
 
 namespace Northwind.Controllers
 {
@@ -17,6 +18,71 @@ namespace Northwind.Controllers
         {
             
             return View(db.Employees.ToList());
+        }
+
+        // GET: Employees/Details/{id}
+        public ActionResult Details(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employees employees = db.Employees.Find(id);
+            if(employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        }
+
+        // Get image file from employees.photo
+        public ActionResult GetImg(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            using (System.IO.MemoryStream ms = new System.IO.MemoryStream())
+            {
+                byte[] image = db.Employees.Where(m => m.EmployeeID == id).Select(m => m.Photo).SingleOrDefault();
+
+                // 78 is the size of the OLE header for Northwind images
+                ms.Write(image, 78, image.Length - 78);
+
+                return File(ms.ToArray(), "image/jpeg");
+            }
+        }
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.ReportsTo = new SelectList(db.Employees, "ReportsTo", "LastName");
+
+            return View();
+        }
+
+        // POST: Employees/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,LastName,FirstName,Title,TitleOfCourtesy,BirthDate,HireDate,Address,City,Region,PostalCode,Country,HomePhone,Extension,Photo,Notes,ReportsTo,PhotoPath")]
+                                    Employees employees)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Employees.Add(employees);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                ViewBag.ReportsTo = new SelectList(db.Employees, "ReportsTo", "LastName", employees.ReportsTo);
+            }
+            catch (Exception e)
+            {
+                System.Diagnostics.Trace.WriteLine("message:\n"+e.Message);
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+
+            return View(employees);
         }
     }
 }
